72edc61ab6d29e34e142bf24808777446f8e70ac
Feora1343 <renee.vorbeck@gmail.com>
2018-02-25 13:06:01 -0600

    Updated pool.js for heroku use

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -2,23 +2,35 @@
 // a generic template that allows a PEAN server to run.
 
 const pg = require('pg');
-const Pool = pg.Pool;
-const config = {
-  database: 'database_name', // the name of the database
-  host: 'localhost', // where is your database
-  port: 5432, // the port number for your database, 5432 is the default
-  max: 10, // how many connections at one time
-  idleTimeoutMillis: 30000 // 30 seconds to try to connect
-};
-const pool = new Pool(config);
+const url = require('url');
+let config = {};
 
-pool.on('connect', (client) => {
-  console.log('Postgesql connected');
-})
+if (process.env.DATABASE_URL) {
+  // Heroku gives a url, not a connection object
+  const params = url.parse(process.env.DATABASE_URL);
+  const auth = params.auth.split(':');
+  
+  config = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: true, // heroku requires ssl to be true
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  };
 
-pool.on('error', (err, client) => {
-  console.log('Unexpected error on idle client', err);
-  process.exit(-1);
-});
+} else {
+  config = {
+    user: process.env.PG_USER || null, //env var: PGUSER
+    password: process.env.DATABASE_SECRET || null, //env var: PGPASSWORD
+    host: process.env.DATABASE_SERVER || 'localhost', // Server hosting the postgres database
+    port: process.env.DATABASE_PORT || 5432, //env var: PGPORT
+    database: process.env.DATABASE_NAME || 'todo_app', //env var: PGDATABASE
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  };
+}
 
-module.exports = pool;
+module.exports = new pg.Pool(config);
